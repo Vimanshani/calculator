3e3b9bd56adfcf457610af097d366f1353dc18e5
Vimanshani <pabodhavimashani@gmail.com>
2023-05-27 11:43:00 +0530

    1

---
+++ first.js
@@ -0,0 +1,8 @@
+function calculate(){
+   //  alert("Button Test")
+     let fn= parseFloat(document.getElementById("txtfirstnumber").value) ;
+     let sn= parseFloat(document.getElementById("txtsecondnumber" ).value);
+ 
+     document.getElementById("txtresult").value= fn+sn;
+
+}
